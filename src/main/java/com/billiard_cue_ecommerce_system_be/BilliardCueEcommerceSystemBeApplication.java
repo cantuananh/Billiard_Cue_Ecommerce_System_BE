e09b64700d46e4bfdc2a78458550d3c998d4e0fc
cantuananh <tuananhthemen391997@gmail.com>
2025-11-06 22:05:18 +0700

    first commit

---
+++ src/main/java/com/billiard_cue_ecommerce_system_be/BilliardCueEcommerceSystemBeApplication.java
@@ -0,0 +1,13 @@
+package com.billiard_cue_ecommerce_system_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BilliardCueEcommerceSystemBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BilliardCueEcommerceSystemBeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/billiard_cue_ecommerce_system_be/BilliardCueEcommerceSystemBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.billiard_cue_ecommerce_system_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BilliardCueEcommerceSystemBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
